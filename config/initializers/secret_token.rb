7e60c2448afb2367f671c7dfd7bf2ddc9b775925
etewiah <etewiah@hotmail.cim>
2012-08-26 04:59:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zzzytology::Application.config.secret_token = 'd3451d92c6d572bfc501a38f6adf15094757aae4688170631a9617145a1d5ab585cf8c4fda6ddb9b700ebe3299e0770c6bd9b6d8ff5fe914d8d3587fa80b74d0'
